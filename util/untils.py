d2beb2db313320d3ad7c6bd2c6248a7aa5b71cc0
JiakangSun1 <155050888+JiakangSun1@users.noreply.github.com>
2025-03-30 00:08:26 +0800

    Add files via upload

---
+++ util/untils.py
@@ -0,0 +1,8 @@
+
+import torch.nn as nn
+def has_batchnorms(model):
+    bn_types = (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d, nn.SyncBatchNorm)
+    for name, module in model.named_modules():
+        if isinstance(module, bn_types):
+            return True
+    return False
